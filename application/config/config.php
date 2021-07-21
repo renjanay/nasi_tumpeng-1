20945849dc42e369dd0e36ffb852b756415f0567
renjanay <ainayyafirda123@gmail.com>
2021-07-21 13:58:50 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/unikom/sem4/RPL/nasi_tumpeng/';
+$config['base_url'] = 'http://localhost/nasi_tumpeng/';
 
 /*
 |--------------------------------------------------------------------------
